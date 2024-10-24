84747e09504b0e3cbd79dd221b9cb1e212f934b2
abdurrahmanabid <muhaabdurrahman@gmail.com>
2024-10-25 00:40:43 +0600

    plane changed

---
+++ src/store/navbarObject.jsx
@@ -0,0 +1,28 @@
+export const consumer = [
+  {
+    name: "Menus",
+    path: "./menus",
+  },
+  {
+    name: "Order",
+    path: "./order",
+  },
+  {
+    name: "Menu",
+    path: "./menu",
+  },
+];
+export const seller = [
+   {
+     name: "Dashboard",
+     path: "./dashboard",
+   },
+   {
+     name: "Order",
+     path: "./order",
+   },
+   {
+     name: "Menu",
+     path: "./menu",
+   },
+ ];
